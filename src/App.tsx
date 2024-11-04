adb4b0afecc28c0abbe793f69f68797cd1dc98b8
MOHAMMED SADIK K P <gibmepreo@gmail.com>
2024-11-04 18:02:45 +0530

    Initial folder structure has done

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header className="bg-blue-500 p-4 text-white text-center">
+        <h1>OLX Clone</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JavaScript and TypeScript files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
